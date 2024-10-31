83ad53c555b01604d0a536edf9418af89517c9e2
Sezgin Sevinc <sezginex@gmail.com>
2024-10-31 08:24:06 +0300

    syntax

--- walnut-news/scraper.js
+++ walnut-news/scraper.js
@@ -2,27 +2,22 @@ import { launch } from 'puppeteer';
 
 async function getNewsTitle() {
     try {
-        // Tarayıcıyı başlat
         const browser = await launch({
             headless: true,
-            // defaultViewport: null
         });
 
-        // Yeni sayfa aç
         const page = await browser.newPage();
 
-        // Sayfa yüklenme durumunu izle
         page.on('load', () => console.log('Sayfa yüklendi'));
 
-        // İlk sayfaya git
         await page.goto('https://trthaber.com', {
             waitUntil: 'domcontentloaded'
         });
 
         const headlineId = await page.evaluate(() => {
             // başlık değişken olabilir 
-            // headline ile başlayan ve Title ile biten ilk id'yi bulun
-            // Örneğin: headline118427Title
+            // headline ile başlayan ve Title ile biten ilk id'yi bul
+            // Örnek: headline118427Title
             const el = document.querySelector('[id^="headline"][id$="Title"]');
             return el ? el.id : null;
         });
@@ -30,8 +25,6 @@ async function getNewsTitle() {
 
         await page.click(`a[href="${linkHref}"]`)
 
-
-        // news-title ve news-content class'ına sahip ilk elementin metnini al
         const newsTitle = await page.evaluate(() => {
             const titleElement = document.querySelector('.news-title');
 
